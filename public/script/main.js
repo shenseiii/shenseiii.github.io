f82970628a4c8bab6a841b012d41ab930b0b0b96
shenseiii <112975267+shenseiii@users.noreply.github.com>
2023-01-25 19:54:56 +0800

    updated

--- public/script/main.js
+++ public/script/main.js
@@ -1,11 +1,7 @@
-<script>
-  const scriptURL = '<https://script.google.com/macros/s/AKfycbxRRc0HmNYYEfBb4iWdXrEXleSc_3qyyqSy-dYFdlOvxiEdkr3ec9t73A5qNHMtwf48/exec>'
-  const form = document.['contact-box']
+function openMenu() {
+  document.getElementById('sideMenu').classList.remove('sidemenu-in');
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
-</script>
+}
+function closeMenu() {
+  document.getElementById('sideMenu').classList.add('sidemenu-in');
+};
